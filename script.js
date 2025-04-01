6a1de28804d78ee6c8a3894e2d654ecba836cc9d
Simen Byremo <simenwb55@gmail.com>
2025-04-01 18:02:49 +0200

    Todo-list finnish

--- script.js
+++ script.js
@@ -9,5 +9,20 @@ function addTask() {
 
     li.innerHTML = inputBox.value;
     listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
   }
+  inputBox.value = "";
 }
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+    }
+  },
+  false
+);
